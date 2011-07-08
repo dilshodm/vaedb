c704f487229035aa38d8e529591f7df0b0ef1831
bombino <bombino@aad4f607-6d21-4a91-bd5e-03100d5e8654>
2011-07-08 08:15:30 +0000

    fix
    git-svn-id: http://svn.datadevelopment.net/actionverb/vae_thrift/trunk@3397 aad4f607-6d21-4a91-bd5e-03100d5e8654

--- php/vaedb_test.php
+++ php/vaedb_test.php
@@ -8,20 +8,14 @@ require_once("/www/vae_remote/current/lib/store.php");
 require_once("/www/vae_remote/current/lib/phpapi.php");
 require_once("/www/vae_remote/current/lib/vaedata.php");
 require_once("/www/vae_remote/current/lib/vae_exception.php");
-$_VAE['settings']['subdomain'] = "btg";
+$_VAE['settings']['subdomain'] = "rxart";
 $_VAE['staging'] = true;
 $_VAE['config']['secret_key'] = "9dee6740e4ddbe2c9488242318b764beeef7a17f";
-//$_VAE['settings']['subdomain'] = "peeptoe";
-//$_VAE['config']['secret_key'] = "9dee6740e4ddbe2c9488242318b764beeef7a17f";
-//$_VAE['settings']['subdomain'] = "hintmagazine";
-//$_VAE['config']['secret_key'] = "8021e63777c6fe00a7a0ab660b29780d0e9fee25";
-//$_VAE['settings']['subdomain'] = "impose";
-//$_VAE['config']['secret_key'] = "4dd583d746bcaaac042a0a132e6971b245fd174f";
 $k = microtime(true);
 
-$items = vae("artists");
-foreach ($items as $r) {
-  echo $r->name . "\n";
+foreach (vae("items") as $id => $item) {
+  echo "Got $id\n";
+  var_dump($item->structure());
 }
 
 echo "\n" . (microtime(true)-$k) . " seconds elapsed\n";
