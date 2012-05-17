791dbb769ad58c6603aceea74d4169b8941e6646
Alfred Rossi <alfredr@gmail.com>
2012-05-16 23:54:22 -0400

    escape strings in the query log

--- cpp/query_log.h
+++ cpp/query_log.h
@@ -4,22 +4,46 @@
 #include <map>
 #include <sstream>
 
-template <typename T> 
+inline
+void str_replace(std::string &str,
+                 std::string const & search,
+                 std::string const & replacement) {
+
+  string::size_type loc = 0;
+  while((loc = str.find(search, loc)) != string::npos)
+    str.replace(loc++, search.size(), replacement);
+}
+
+template <typename T>
 struct TypeHelper {
-  static std::string Name();
-  static std::string ValueRep(const T & val);
+  inline static std::string Name();
+  inline static std::string ValueRep(const T & val);
+};
+
+template <>
+struct TypeHelper<std::string> {
+  inline static std::string Name() {
+    return "string";
+  }
+  inline static std::string ValueRep(const std::string & val) {
+    std::stringstream ss;
+    std::string escaped(val);
+    str_replace(escaped, "\\", "\\\\");
+    str_replace(escaped, "\"", "\\\"");
+    ss << '"' << escaped << '"';
+    return ss.str();
+  }
 };
 
 template <>
 template <typename K, typename V>
 struct TypeHelper<std::map<K,V> > {
-  static std::string Name() {
+  inline static std::string Name() {
     return std::string("map<"+TypeHelper<K>::Name()+","+TypeHelper<V>::Name()+">");
-  } 
+  }
 
-  static std::string ValueRep(const std::map<K,V> & val) {
+  inline static std::string ValueRep(const std::map<K,V> & val) {
     std::stringstream ss;
-
     ss << "{";
 
     typename std::map<K,V>::const_iterator iter = val.begin();
@@ -31,23 +55,22 @@ struct TypeHelper<std::map<K,V> > {
   }
 };
 
-#define DEFINE_SIMPLE_LOGNAME(T, name)         \
-template <>                                    \
-struct TypeHelper< T > {                       \
-  static std::string Name() {                  \
-    return name ;                              \
-  }                                            \
-  static std::string ValueRep(const T & val) { \
-    std::stringstream ss;                      \
-    ss << val;                                 \
-    return ss.str();                           \
-  }                                            \
-}                                              \
+#define DEFINE_SIMPLE_LOGNAME(T, name)                \
+template <>                                           \
+struct TypeHelper< T > {                              \
+  inline static std::string Name() {                  \
+    return name ;                                     \
+  }                                                   \
+  inline static std::string ValueRep(const T & val) { \
+    std::stringstream ss;                             \
+    ss << val;                                        \
+    return ss.str();                                  \
+  }                                                   \
+}                                                     \
 
 
 DEFINE_SIMPLE_LOGNAME(bool, "bool");
 DEFINE_SIMPLE_LOGNAME(int32_t, "i32");
-DEFINE_SIMPLE_LOGNAME(std::string, "string");
 
 struct QueryLog {
   QueryLog(ostream * p_out) : _p_out(p_out) {}
@@ -62,7 +85,7 @@ struct QueryLog {
   template <typename T>
   QueryLog & operator<< (const T & value) {
     if(_p_out)
-      (*_p_out) << "  " << TypeHelper<T>::Name() << ":" 
+      (*_p_out) << "  " << TypeHelper<T>::Name() << ":"
                         << TypeHelper<T>::ValueRep(value) << std::endl;
     return *this;
   }
