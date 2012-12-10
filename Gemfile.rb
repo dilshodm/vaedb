1f0c3dbd8776380a6f6b636e9afdff8e948141c2
Arthur Neuman <arthur.neuman@gmail.com>
2012-12-10 12:32:36 -0500

    Starting switch to captionverb and multistage deployment

---
+++ Gemfile.rb
@@ -0,0 +1,7 @@
+source :rubygems
+source 'https://gems.gemfury.com/cgSsDV8i4yL4mnuN8zk7/'
+
+gem 'captionverb'
+gem 'capistrano', :require => nil
+gem 'capistrano-ext', :require => nil
+gem 'whenever', :require => nil
