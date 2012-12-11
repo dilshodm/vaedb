21d43bb071e9b8fa4a011ed237f3df13edb74e44
Arthur Neuman <arthur.neuman@gmail.com>
2012-12-11 11:42:04 -0500

    Fixing extension on Gemfile

--- Gemfile.rb
+++
@@ -1,7 +0,0 @@
-source :rubygems
-source 'https://gems.gemfury.com/cgSsDV8i4yL4mnuN8zk7/'
-
-gem 'captionverb'
-gem 'capistrano', :require => nil
-gem 'capistrano-ext', :require => nil
-gem 'whenever', :require => nil
