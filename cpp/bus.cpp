c12dff0d686990e7a41f57d873c8b3ebd89ab8c5
Alfred Rossi <alfred@actionverb.com>
2014-03-06 12:28:37 -0500

    reload downloads take place in their own threads

--- cpp/bus.cpp
+++ cpp/bus.cpp
@@ -1,6 +1,9 @@
 #include <map>
 #include <string>
+#include <vector>
 #include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 
 using namespace std;
 using namespace boost;
@@ -15,12 +18,18 @@ using std::string;
 Bus::Bus(shared_ptr<VaeDbHandler> handler, string bindaddress) 
     : _handler(handler), _bindaddress(bindaddress) { }
 
+void Bus::reload(string subdomain) {
+  _handler->reloadSite(subdomain);
+}
+
 void Bus::run() {
   zmq::context_t context(1);
   zmq::socket_t subscriber(context, ZMQ_PULL);
+  vector<boost::thread*> threads;
+  
+  boost::posix_time::seconds waittime(0);
 
   subscriber.bind(_bindaddress.c_str());
-
   while(true) {
     zmq::message_t update;
     subscriber.recv(&update);
@@ -29,5 +38,18 @@ void Bus::run() {
 
     L(info) << "reloading " << subdomain;
     _handler->reloadSite(subdomain);
+
+    vector<boost::thread*>::iterator it = threads.begin();
+    while(it != threads.end()) {
+        if((*it)->timed_join(waittime)) {
+            delete *it;
+            it = threads.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
+
+    threads.push_back(new boost::thread(&Bus::reload, this, subdomain));
   }
 }
