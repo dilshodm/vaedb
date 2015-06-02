e5475c40c842c1a134da14126bd04ffcd0af361a
Kevin Bombino <kevin@bombino.org>
2015-06-01 22:04:59 -0700

    connection pool for memcached too

--- cpp/memcache_proxy.cpp
+++ cpp/memcache_proxy.cpp
@@ -6,18 +6,54 @@
 
 using std::string;
 
-MemcacheProxy::MemcacheProxy(std::string connectString) {
-  pcrecpp::RE(",").Replace(" --SERVER=", &connectString);
-  connectString = "--TCP-KEEPALIVE --SERVER=" + connectString;
+MemcacheProxy::MemcacheProxy(std::string c) {
+  pcrecpp::RE(",").Replace(" --SERVER=", &c);
+  connectString = "--TCP-KEEPALIVE --SERVER=" + c;
   L(info) << " Connecting to Memcache via: " << connectString;
-  client = new memcache::Memcache(connectString);
+  for (int i = 0; i < MEMCACHE_NUM_CONNECTIONS; i++) {
+    memcache::Memcache *con = this->createConnection();
+    if (con) {
+      connPool[con] = MEMCACHE_POOL_FREE;
+      this->availableConnections++;
+    }
+  }
 }
 
 MemcacheProxy::~MemcacheProxy() {
 }
 
+memcache::Memcache *MemcacheProxy::createConnection() {
+  return new memcache::Memcache(connectString);
+}
+
+boost::shared_ptr<memcache::Memcache> MemcacheProxy::getConnection() {
+  boost::unique_lock<boost::mutex> lock(poolMutex);
+  while (this->availableConnections == 0) {
+    this->cond.wait(lock);
+  }
+  for (MemcachePool::iterator iter = connPool.begin(); iter != connPool.end(); iter++) {
+    if (iter->second == MEMCACHE_POOL_FREE) {
+      iter->second = MEMCACHE_POOL_INUSE;
+      this->availableConnections--;
+      return boost::shared_ptr<memcache::Memcache>(iter->first, std::bind(&MemcacheProxy::freeConnection, this, std::placeholders::_1));
+    }
+  }
+  throw "Memcache Thread Pool Error: Should never get here";
+}
+
+void MemcacheProxy::freeConnection(memcache::Memcache *con) {
+  boost::unique_lock<boost::mutex> lock(poolMutex);
+  for (MemcachePool::iterator iter = connPool.begin(); iter != connPool.end(); iter++) {
+    if (iter->first == con) {
+      iter->second = MEMCACHE_POOL_FREE;
+      this->availableConnections++;
+    }
+  }
+  cond.notify_one();
+}
+
 string MemcacheProxy::get(const string key, const int32_t flags) {
-  boost::unique_lock<boost::mutex> lock(clientMutex);
+  boost::shared_ptr<memcache::Memcache> client = this->getConnection();
   std::vector<char> ret_value;
   client->get(key, ret_value);
   string string_ret_value(ret_value.begin(), ret_value.end());
@@ -25,11 +61,12 @@ string MemcacheProxy::get(const string key, const int32_t flags) {
 };
 
 void MemcacheProxy::set(const string key, const string value, const int32_t flags, const int32_t expireInterval) {
-  boost::unique_lock<boost::mutex> lock(clientMutex);
+  boost::shared_ptr<memcache::Memcache> client = this->getConnection();
   const std::vector<char> valueVector(value.begin(), value.end());
   client->set(key, valueVector, expireInterval, flags);
 };
 
 void MemcacheProxy::remove(const string key) {
+  boost::shared_ptr<memcache::Memcache> client = this->getConnection();
   client->remove(key);
 };
--- cpp/memcache_proxy.h
+++ cpp/memcache_proxy.h
@@ -1,13 +1,27 @@
 #ifndef _VAE_THRIFT_MEMCACHE_PROXY_H_
 #define _VAE_THRIFT_MEMCACHE_PROXY_H_
 
-#include <boost/thread/mutex.hpp>
 #include <string>
+#include <boost/smart_ptr.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition_variable.hpp>
 #include <libmemcached/memcached.hpp>
 
+#define MEMCACHE_NUM_CONNECTIONS 16
+
+enum {
+  MEMCACHE_POOL_FREE,
+  MEMCACHE_POOL_INUSE
+};
+
+typedef std::map<memcache::Memcache *, short> MemcachePool;
+
 class MemcacheProxy {
-  memcache::Memcache *client;
-  boost::mutex clientMutex;
+  std::string connectString;
+  MemcachePool connPool;
+  boost::mutex poolMutex;
+  boost::condition_variable cond;
+  int availableConnections;
  
 public:
   MemcacheProxy(std::string connectString);
@@ -15,6 +29,11 @@ public:
   std::string get(const std::string key, const int32_t flags);
   void set(const std::string key, const std::string value, const int32_t flags, const int32_t expireInterval);
   void remove(const std::string key);
+
+private:
+  memcache::Memcache *createConnection();
+  boost::shared_ptr<memcache::Memcache> getConnection();
+  void freeConnection(memcache::Memcache *con);
 };
 
 #endif
